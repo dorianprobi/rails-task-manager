6ff50745cc78032942776ea150788f93e305576e
Dorian Probstmeier <dorian@probstmeier.de>
2018-08-15 09:14:47 +0200

    task-manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @newTask = Task.new
+  end
+
+  def create
+    Task.create!(tasks_params)
+    redirect_to root_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:details, :title, :completed)
+  end
+
 end
